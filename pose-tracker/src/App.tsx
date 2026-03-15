16057ef5e26b8f0c4f621f48a2c78d8119e69c96
ThamadiK <s4108774@student.rmit.edu.au>
2026-03-15 11:50:16 +1100

    Added the ExerciseSelector page

--- pose-tracker/src/App.tsx
+++ pose-tracker/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Landing from "./pages/Landing";
 import Demos from "./pages/Demo";
 import DanceSelector from "./pages/DanceSelector";
+import ExerciseSelector from "./pages/ExerciseSelector";
 import Profile from "./pages/Profile"
 
 
@@ -10,6 +11,10 @@ function App() {
 
   if (page === "dance") {
     return <DanceSelector setPage={setPage} />;
+  }
+
+  if (page === "exercise") {
+    return <ExerciseSelector setPage={setPage} />;
   }
 
   if (page === "demo") {
