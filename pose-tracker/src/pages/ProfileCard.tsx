56713027da34fda257ad51e44b4a49fa603a6a8b
s4111555shahira <s4111555@student.rmit.edu.au>
2026-03-15 15:25:14 +1100

    Profile page updates

--- pose-tracker/src/pages/ProfileCard.tsx
+++ pose-tracker/src/pages/ProfileCard.tsx
@@ -1,43 +1,39 @@
 import Pencil from "../assets/Pencil.png";
-import OldWoman from "../assets/OldWoman.jpg";
+import ProfilePic from "../assets/profilepic.png";
 
 export default function ProfileCard() {
-    return (
-      <div className="profile-card">
-  
-        <div className="profile-left">
-          <img src={OldWoman} alt="Oldwoman avatar" className="OldWoman" />
+  return (
+    <div className="profile-card">
+      <div className="profile-left">
+        <div className="avatar">
+          <img src={ProfilePic} alt="Profile picture" />
         </div>
-  
-        <div className="profile-right">
-  
-          <div className="profile-header">
-            <div>
-              <h2>Lily James</h2>
-              <p>67, Medical condition: back injuries</p>
-            </div>
-            <img src={Pencil} alt="Edit logo" className="Edit" />
+      </div>
 
+      <div className="profile-right">
+        <div className="profile-header">
+          <div>
+            <h2>Lily James</h2>
+            <p>67, Medical condition: back injuries</p>
           </div>
-  
-        <div className= "level-container">
-         <div className="level">lvl. 25</div>
+          <img src={Pencil} alt="Edit logo" className="Edit" />
+        </div>
+
+        <div className="level-container">
+          <div className="level">lvl. 25</div>
           <div className="progress-bar">
             <div className="progress-fill"></div>
           </div>
-          </div>
-  
-          <div className="motions">
-            <div className="motions-icon">⚑</div>
-  
-            <div>
-              <h3>30</h3>
-              <p>Motions done</p>
-            </div>
-          </div>
-  
         </div>
-  
+
+        <div className="motions">
+          <div className="motions-icon">⚑</div>
+          <div>
+            <h3>30</h3>
+            <p>Motions done</p>
+          </div>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
